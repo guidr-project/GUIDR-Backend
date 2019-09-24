79b484087dea5353e03b1c15fd0b1b09a930e65f
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-24 11:00:19 -0700

    added routes, and authentication

--- trips/tripModels.js
+++ trips/tripModels.js
@@ -2,8 +2,18 @@ const db = require('../database/db-config.js')
 
 module.exports = {
     getAllTrips,
+    updateTrip,
+    deleteTrip
 }
 
 function getAllTrips() {
     return db('trips').where({'trips.private': 0})
+}
+
+function updateTrip(changes, id) {
+    return db('trips').update(changes).where({id}).returning('*')
+}
+
+function deleteTrip(id) {
+    return db('trips').where({id}).del()
 }
