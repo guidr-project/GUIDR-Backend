c8cdf2fa27dbeb5ebb29a7d2dc1a27c080aabad6
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-24 18:10:17 -0400

    Build out README docs for using endpoints, and fine tuned what data is returned from certain requests

--- trips/tripRoutes.js
+++ trips/tripRoutes.js
@@ -1,40 +1,41 @@
-const express = require('express')
-const Trips = require('./tripModels.js')
+const express = require('express');
+const Trips = require('./tripModels.js');
+const restricted = require('../auth/authRestricted.js');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
     Trips.getAllTrips()
-    .then(trips => {
-        res.status(200).json(trips)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+        .then(trips => {
+            res.status(200).json(trips);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 router.put('/:id', (req, res) => {
-    const id = req.params.id
-    const changes = req.body
+    const id = req.params.id;
+    const changes = req.body;
 
     Trips.updateTrip(changes, id)
-    .then(trip => {
-        res.status(200).json(trip)
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+        .then(trip => {
+            res.status(200).json(trip);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 router.delete('/:id', (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     Trips.deleteTrip(id)
-    .then(response => {
-        res.status(200).json({message: "Trip successfully deleted"})
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
-})
+        .then(response => {
+            res.status(200).json({ message: 'Trip successfully deleted' });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
