1ee9fa7577c5e0f123ee806cd71b20bd1218f3c7
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-26 14:29:44 -0700

    chaneged return of all trips

--- trips/tripModels.js
+++ trips/tripModels.js
@@ -7,7 +7,18 @@ module.exports = {
 }
 
 function getAllTrips() {
-    return db('trips').where({'trips.private': 0})
+    return db('users as u')
+    .join('trips as t', 'u.id', 't.user_id')
+    .select('u.full_name', 't.id', 't.title', 't.description', 't.private', 't.type', 't.start_date', 't.end_date', 't.duration_hours', 't.duration_days')
+    .where({'t.private': 0})
+    .then(trips => {
+        const privateBoolean = trips.map(trip => {
+            trip.private = Boolean(trip.private);
+            return trip;
+        });
+        return privateBoolean;
+    });
+    
 }
 
 function updateTrip(changes, id) {
