c264529dc72a40b504dc592972736d84f491332f
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2020-02-18 23:17:16 -0500

    Minor syntax corrections

--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors')
 const helmet = require('helmet')
 const userRouter = require('../user/userRoutes.js');
-const tripRoutes = require('../trips/tripRoutes.js')
+const tripRoutes = require('../trips/tripRoutes.js');
 
 const server = express();
 
 server.use(express.json());
-server.use(helmet())
-server.use(cors())
+server.use(helmet());
+server.use(cors());
 server.use('/users', userRouter);
 server.use('/trips', tripRoutes);
 
